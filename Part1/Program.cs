aebac262f612d961120fbf3d5ae4178bae748cf9
Herczku Misi <kmisku@gmail.com>
2016-04-18 20:12:59 +0200

    Solve part1

---
+++ Part1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+/// <summary>
+/// Create a program that organizes the strings given by the user! Use the ArrayList collection!
+/// </summary>
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList strings = new ArrayList();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + " Write here a string: ");
+                //strings.Insert(0, Console.ReadLine());
+                strings.Add(Console.ReadLine());
+            }
+
+            Console.ReadKey();
+
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
